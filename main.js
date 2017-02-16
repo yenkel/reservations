2e8de59b6a2bab662735582e692a770e8b7b01e1
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-02-16 11:32:28 +0200

    update

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var reservations = {
-  'Bob': { claimed: false },
-  'Ted': { claimed: true }
+    'Bob': { claimed: false },
+    'Ted': { claimed: true }
 }
 
 var name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
-  // write your code here!
+var claimReservation = function() {
+    // write your code here!
+    if (reservations[name]) {
+        if (reservations[name].claimed === false) {
+            alert("Welcome " + name + " Please follow me");
+        } else {
+            alert("Hi " + name + " Someone is waiting for you ");
+        }
+    } else {
+        reservations[name] = { claimed: true }
+        alert("Hi " + name + " a new reservation has been made for you");
+    }
 }
 
 claimReservation();
